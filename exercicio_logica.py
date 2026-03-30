930c8d8e7c708f6bb3141aa9ff95768a28758f7c
emillyvolvr <emillydovale817@gmail.com>
2026-03-30 14:11:40 -0300

    exercicio_logica

---
+++ exercicio_logica.py
@@ -0,0 +1,10 @@
+y = 2
+
+# primeira expressao
+condicao1 = 4 * y * y + 3 * y - 5 >= 0
+
+# segunda expressao 
+condicao2 = 2 * y > 3 or (y - 1) < 0
+
+print("A condição 1 é:", condicao1)
+print("A condição 2 é:", condicao2)
